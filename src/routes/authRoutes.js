ba019f31f59df18a27f29f346a1eb604d4c2ad0c
Nii Commey <jude.niicommey@outlook.com>
2026-03-07 22:14:11 +0000

    assignment submission

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,22 +5,19 @@ import User from "../models/User.js";
 
 const router = express.Router();
 
-// POST /api/auth/register
 router.post("/register", async (req, res) => {
-  // - Return user (without password)
-
   try {
 
     const { name, email, password } = req.body
 
-    // - Validate input
+    
     if (!name || !email || !password){
       return res.status(400).json({
         message: "All fields are required"
       })
     }
 
-    // - Check if user exists
+    
     const existingUser = await User.findOne({email})
 
     if (existingUser){
@@ -29,11 +26,11 @@ router.post("/register", async (req, res) => {
       })
     }
 
-     // - Hash password
+    
      const salt=10
      const hashedPassword = await bcrypt.hash(password, salt)
 
-     // - Save user
+   
      const user = new User({
       name:name,
       email:email,
@@ -42,7 +39,7 @@ router.post("/register", async (req, res) => {
 
     await user.save()
 
-    // - Return user (without password)
+    
     return res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -58,21 +55,21 @@ router.post("/register", async (req, res) => {
 
 });
 
-// POST /api/auth/login
+
 router.post("/login", async (req, res) => {
 
   try {
     
     const { email , password } = req.body
 
-    // Validate input
+    
     if (!email || !password) {
       return res.status(400).json({
         message: "Email and password required"
       });
     }
 
-    // - Find user
+    
     const user = await User.findOne({ email })
 
     if (!user) {
@@ -81,7 +78,6 @@ router.post("/login", async (req, res) => {
       })
     }
 
-    // - Compare password
     const match = await bcrypt.compare(password, user.password)
 
     if (!match) {
@@ -90,13 +86,13 @@ router.post("/login", async (req, res) => {
       })
     }
 
-    // - Generate JWT
+    
     const token = jwt.sign(
       { userId: user._id },
       process.env.JWT_SECRET,
       { expiresIn: "1d" })
 
-    // - Return token
+    
     return res.status(200).json({ token });
 
   } catch (error) {
